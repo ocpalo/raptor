a6aa6ee034c88b7017de7ebd83be42007fc8ca32
Berke <iamberkeyavas@gmail.com>
2021-09-24 01:20:36 +0300

    post method now takes interface{} as argument

--- goClient/main.go
+++ goClient/main.go
@@ -29,6 +29,7 @@ func getApiServerTime() (time ServerTime, err error) {
 	return serverTime, err
 }
 
+// DEPRECATED
 func postApiLogin() (int , error) {
 	body, err := json.Marshal(loginInfo)
 	if err != nil {
@@ -49,6 +50,7 @@ func postApiLogin() (int , error) {
 	return resp.StatusCode, err
 }
 
+// DEPRECATED
 func postApiLockInfo() (int, error) {
 	body, err := json.Marshal(lockInfo)
 	if err != nil {
@@ -90,6 +92,28 @@ func postApiTelemetry() (TelemetryResponse ,error) {
 	return telemetryResp, err
 }
 
+// use this instead of postApiLockInfo, postApiLogin
+// needs to be tested
+func postApi(api interface{}) (int, error) {
+	body, err := json.Marshal(api)
+	if err != nil {
+		return 0, err
+	}
+
+	resp, err := http.Post("", "", bytes.NewBuffer(body))
+	if err != nil {
+		return 0, err
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}(resp.Body)
+
+	return resp.StatusCode, err
+}
+
 
 func main() {
 	time, err := getApiServerTime()
@@ -97,6 +121,10 @@ func main() {
 	login, err := postApiLogin()
 	fmt.Println(login, err)
 	lock, err := postApiLockInfo()
+	fmt.Println(lock, err)
+	login, err = postApi(loginInfo)
+	fmt.Println(login, err)
+	lock, err = postApi(lockInfo)
 	fmt.Println(lock, err)
 	telem, err := postApiTelemetry()
 	fmt.Println(telem, err)
