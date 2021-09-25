2ba508baad10da4ef31e746f8a5e221d34f6c22c
Berke <iamberkeyavas@gmail.com>
2021-09-25 03:42:08 +0300

    made a package for POST

--- goClient/client_api.go
+++ goClient/client_api.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 )
 
+// TODO:: Modularize after testing POST
 func getApiServerTime() (time ServerTime, err error) {
 	resp, err := http.Get("")
 	if err != nil {
@@ -29,49 +29,3 @@ func getApiServerTime() (time ServerTime, err error) {
 	return serverTime, err
 }
 
-// TODO::remove error status in the future, nil check is sufficient
-func post(api interface{}) (*http.Response, error) {
-	body, err := json.Marshal(api)
-	if err != nil {
-		return nil, err
-	}
-
-	resp, err := http.Post("", "", bytes.NewBuffer(body))
-	if err != nil {
-		return nil, err
-	}
-
-	return resp, nil
-}
-
-func DecodeHttpStatus(response *http.Response) int {
-	return response.StatusCode
-}
-
-func DecodeResponseBody(response *http.Response, str interface{}) error{
-	err := json.NewDecoder(response.Body).Decode(&str)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func Post(api interface{}, apiResponse interface{}) int {
-	post, err := post(&api)
-	if err != nil {
-		return 0
-	}
-	defer func(Body io.ReadCloser) {
-		err := Body.Close()
-		if err != nil {
-			fmt.Println("HANDLE ME")
-		}
-	}(post.Body)
-
-	status := DecodeHttpStatus(post)
-	err = DecodeResponseBody(post, &apiResponse)
-	if err != nil {
-		return 0
-	}
-	return status
-}
--- goClient/main.go
+++ goClient/main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"fmt"
 	_ "github.com/go-co-op/gocron"
+	 "uav_client/src/post"
 )
 
 
 func main() {
 	resp, err := getApiServerTime()
 	fmt.Println(resp, err)
-	var telemetryResponse TelemetryResponse
-	status := Post(&telemetry, &telemetryResponse)
-	fmt.Println(status, telemetryResponse)
+	status := post.Post(&telemetry, &telemetryResp)
+	fmt.Println(status, telemetryResp)
 }
